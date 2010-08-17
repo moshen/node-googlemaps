c090b371324cea3ab098f491b56b87015582694f
Colin <moshen.colin@gmail.com>
2010-08-17 14:24:27 -0500

    Created some tests using vows Fixed typos in reverseGeocode Callbacks now return an error (as err) Updated README with changes and TODOs

---
+++ test/geocode.js
@@ -0,0 +1,95 @@
+var vows = require('vows'),
+	assert = require('assert'),
+	gm = require('../lib/googlemaps');
+
+vows.describe('geocode').addBatch({
+	'Simple geocode (Chicago)': {
+		topic: function(){
+			gm.geocode('Chicago , Il , USA', this.callback, 'false');
+		},
+		'returns as a valid request': function(err, result){
+			assert.equal(result.status , 'OK');
+		},
+		'returns the expected lat/lng for Chicago': function(err, result){
+			assert.equal(result.results[0].geometry.location.lat , 41.850033);
+			assert.equal(result.results[0].geometry.location.lng , -87.6500523);
+		}
+	}
+}).export(module);
+
+
+/*  Geocode query results
+{
+   "status":"OK",
+   "results":[
+      {
+         "types":[
+            "locality",
+            "political"
+         ],
+         "formatted_address":"Chicago, IL, USA",
+         "address_components":[
+            {
+               "long_name":"Chicago",
+               "short_name":"Chicago",
+               "types":[
+                  "locality",
+                  "political"
+               ]
+            },
+            {
+               "long_name":"Cook",
+               "short_name":"Cook",
+               "types":[
+                  "administrative_area_level_2",
+                  "political"
+               ]
+            },
+            {
+               "long_name":"Illinois",
+               "short_name":"IL",
+               "types":[
+                  "administrative_area_level_1",
+                  "political"
+               ]
+            },
+            {
+               "long_name":"United States",
+               "short_name":"US",
+               "types":[
+                  "country",
+                  "political"
+               ]
+            }
+         ],
+         "geometry":{
+            "location":{
+               "lat":41.850033,
+               "lng":-87.6500523
+            },
+            "location_type":"APPROXIMATE",
+            "viewport":{
+               "southwest":{
+                  "lat":41.7169105,
+                  "lng":-87.9061711
+               },
+               "northeast":{
+                  "lat":41.982879,
+                  "lng":-87.3939335
+               }
+            },
+            "bounds":{
+               "southwest":{
+                  "lat":41.644335,
+                  "lng":-87.9402669
+               },
+               "northeast":{
+                  "lat":42.023131,
+                  "lng":-87.5236609
+               }
+            }
+         }
+      }
+   ]
+}
+*/
