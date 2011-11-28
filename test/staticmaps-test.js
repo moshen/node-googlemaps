262eba0953509b2f24636c62cab5521eb18e8d65
Josh Smith <joshdotsmith@gmail.com>
2011-11-28 15:40:28 -0500

    Added in styles for Static Maps Signed-off-by: Josh Smith <joshdotsmith@gmail.com>

--- test/staticmaps-test.js
+++ test/staticmaps-test.js
@@ -6,10 +6,16 @@ var assert = require('assert'),
 //}).export(module);
 
 markers = [
-	{ 'location': '440 W Main St Lock Haven, PA', 'color': 'blue', 'label': 'C', 'size': 'tiny' },
+	{ 'location': '300 W Main St Lock Haven, PA' },
 	{ 'location': '444 W Main St Lock Haven, PA', 'color': 'red', 'label': 'A' },
+]
+
+styles = [
+	{ 'feature': 'road', 'element': 'all', 'rules': 
+		{ 'hue': '0x00ff00' }
+	}
 ]
 
 gm.staticMap('444 W Main St Lock Haven PA', 15, '500x400', false, function(error, results) {
 	console.log(results);
-}, 'roadmap', markers);
+}, 'roadmap', markers, styles);
