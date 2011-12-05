7685d18c7959e30d367f74cf486a63e270300399
Colin <moshen.colin@gmail.com>
2011-12-04 18:29:59 -0600

    Code style and var fixes for staticmaps api addition
    Most changes were to make jshint happy.
    For example:
      - using dot notation for objects
      - inserting semicolons where appropriate
    Also spaced things out for readability
    Fixed missing paths var in functino definition

--- test/staticmaps-test.js
+++ test/staticmaps-test.js
@@ -27,7 +27,7 @@ vows.describe('staticmaps').addBatch({
 			    }
 			]
 
-			gm.staticMap('444 W Main St Lock Haven PA', 15, '500x400', false, this.callback, 'roadmap', markers, styles);
+			gm.staticMap('444 W Main St Lock Haven PA', 15, '500x400', this.callback, false, 'roadmap', markers, styles, paths);
 		},
 		'returns the expected static map URL': function(result, err){
 			assert.equal(result , "http://maps.googleapis.com/maps/api/staticmap?center=444%20W%20Main%20St%20Lock%20Haven%20PA&zoom=15&size=500x400&maptype=roadmap&markers=%7C300%20W%20Main%20St%20Lock%20Haven%2C%20PA&markers=%7Ccolor%3Ared%7Clabel%3AA%7Cicon%3Ahttp%3A%2F%2Fchart.apis.google.com%2Fchart%3Fchst%3Dd_map_pin_icon%26chld%3Dcafe%257C996600%7Cshadow%3Afalse%7C444%20W%20Main%20St%20Lock%20Haven%2C%20PA&style=%7Cfeature%3Aroad%7Celement%3Aall%7Chue%3A0x00ff00&path=weight%3A5%7Ccolor%3A0x0000ff%7C41.139817%2C-77.454439%7C41.138621%2C-77.451596&sensor=false");
