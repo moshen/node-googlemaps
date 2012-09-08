d521b016df6ff542748e1ebd17276b0317fa76b0
Joe Longstreet <joelongstreet@gmail.com>
2012-09-08 07:48:02 -0500

    changing convert namespace and adding a test for reverse geocoding

--- test/conversion-test.js
+++ test/conversion-test.js
@@ -3,24 +3,38 @@ var vows = require('vows'),
   gm = require('../lib/googlemaps');
 
 vows.describe('geocode').addBatch({
-  'Data Conversion (Kansas City)': {
+  'Geocode Conversion (Kansas City)': {
     topic: function(){
       gm.geocode('64111', this.callback, 'false');
     },
     'can convert zip to state': function(err, result){
-      gm.convert(result, 'state', function(data){
+      gm.utils.convert(result, 'state', function(data){
         assert.equal(data , 'Missouri');
       })
     },
     'can convert zip to country': function(err, result){
-      gm.convert(result, 'country', function(data){
+      gm.utils.convert(result, 'country', function(data){
         assert.equal(data , 'United States');
       })
     },
     'can convert zip to geo': function(err, result){
-      gm.convert(result, 'geo', function(data){
+      gm.utils.convert(result, 'geo', function(data){
         assert.equal(data.lat , '39.0587452');
         assert.equal(data.lng , '-94.5985613');
+      })
+    }
+  }
+}).export(module);
+
+
+vows.describe('geocode').addBatch({
+  'ReverseGeocode Conversion (Kansas City)': {
+    topic: function(){
+      gm.reverseGeocode('39.0587452,-94.5985613', this.callback, 'false');
+    },
+    'can convert geo to state': function(err, result){
+      gm.utils.convert(result, 'state', function(data){
+        assert.equal(data , 'Missouri');
       })
     }
   }
