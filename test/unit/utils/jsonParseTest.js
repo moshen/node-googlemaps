e949d543fef2ab7098656db942b1e4fc1ff2747e
Kirk Strobeck <kirk@strobeck.com>
2016-07-16 20:48:05 -0700

    Bump `request` per security warning
    - Request is now https://github.com/request/request/tree/v2.73.0
    
    - Vulnerability https://nodesecurity.io/advisories/77
      Path: googlemaps@1.6.0 > request@2.44.0 > hawk@1.1.1

--- test/unit/utils/jsonParseTest.js
+++ test/unit/utils/jsonParseTest.js
@@ -31,11 +31,11 @@ describe('jsonParser', function() {
       var parser = jsonParser(function(err, jsonObj) {
         should.not.exist(jsonObj);
         should.exist(err);
-        err.message.should.equal('Unexpected token i');
+        err.message.should.equal('Unexpected token i in JSON at position 0');
       });
 
       parser(null, 'i am an invalid json string');
-    
+
     });
 
     it('should call the callback only once if an error is thrown in callback execution', function() {
