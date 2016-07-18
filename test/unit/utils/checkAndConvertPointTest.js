d9af12a792bbe49d188621fda08919523934dbd3
Fabrizio Moscon <mosconfabrizio@gmail.com>
2016-07-19 00:16:05 +0100

    fix tests

--- test/unit/utils/checkAndConvertPointTest.js
+++ test/unit/utils/checkAndConvertPointTest.js
@@ -3,40 +3,37 @@ var should = require('should'),
   gm = new GoogleMapsAPI();
 
 describe('checkAndConvertPoint', function() {
+
   describe('Using a lat/lng point as a string', function() {
-    var result = gm.checkAndConvertPoint('41.874929479660025,-87.62077331542969');
+    var input = '41.874929479660025,-87.62077331542969'
     it('should equal to expected string value', function() {
-      should.equal(result, '41.874929479660025,-87.62077331542969');
+      gm.checkAndConvertPoint(input).should.equal(input);
     });
   });
 
   describe('Using a lat/lng point as an array of numbers', function() {
-    var result = gm.checkAndConvertPoint([41.874929479660025, -87.62077331542969]);
+    var lat = 41.874929479660025
+    var lng = -87.62077331542969
+    var input = [lat, lng];
     it('should equal to expected string value', function() {
-      should.equal(result, '41.874929479660025,-87.62077331542969');
+      gm.checkAndConvertPoint(input).should.eql(lat + ',' + lng);
     });
   });
 
   describe('Using a lat/lng point as a mixed array', function() {
-    var result = gm.checkAndConvertPoint([41.874929479660025, ['-87.62077331542969']]);
+    var lat = 41.874929479660025
+    var lng = ['-87.62077331542969']
     it('result is equal to expected string value', function() {
-      should.equal(result, '41.874929479660025,-87.62077331542969');
+      gm.checkAndConvertPoint([lat, lng]).should.equal(lat + ',' + lng[0]);
     });
   });
 
   describe('Using incorrect lat/lng input (an object)', function() {
-    var result;
-    try{
-      result = gm.checkAndConvertPoint({'lat': 41.874929479660025, 'lng': -87.62077331542969});
-    }catch(e){
-      result = e;
-    }
-
-    it('exception caught is an Error', function() {
-      should(result).be.Error();
-    });
-    it('error thrown was checkAndConvertPoint\'s error', function() {
-      should.ok(result.message.search('checkAndConvertPoint') > 0);
+    it ('should throw', function() {
+      (function(){
+        gm.checkAndConvertPoint({'lat': 41.874929479660025, 'lng': -87.62077331542969});
+      }).should.throw('Unrecognized input: checkAndConvertPoint accepts Arrays of Numbers and Strings')
     });
   });
+
 });
